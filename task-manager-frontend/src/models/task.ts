a91278004637ebeda4e376a1114f85fcf63463da
Mirraely <mirraelymoura@gmail.com>
2025-02-03 23:16:47 -0300

    Permite a adição de uma nova tarefa.

--- task-manager-frontend/src/models/task.ts
+++ task-manager-frontend/src/models/task.ts
@@ -1,8 +1,8 @@
 export interface Task {
-  id: string;
+  id?: string;
   title: string;
   description?: string;
   status: "pending" | "completed";
-  createdAt: string;
+  createdAt?: string;
 }
 
--- task-manager-frontend/src/pages/Home/index.tsx
+++ task-manager-frontend/src/pages/Home/index.tsx
@@ -5,31 +5,46 @@ import Modal from "../../components/Modal";
 import { Task } from "../../models/task";
 
 function Home() {
-  const [activePage, setActivePage] = useState("list"); 
-  const [task, setTask] = useState<Task|null>(null); 
+  const [activePage, setActivePage] = useState("list");
+  const [task, setTask] = useState<Task | null>(null);
   const [modalOpen, setModalOpen] = useState(false);
 
-    // Função para abrir o modal de nova tarefa
-    const openModal = () => {
-      setModalOpen(true);
-    };
-  
-    // Função para fechar o modal de nova tarefa
-    const closeModal = () => {
-      setModalOpen(false);
-    };
+  // Função para abrir o modal de nova tarefa
+  const openModal = () => {
+    setModalOpen(true);
+  };
 
-    const handleSetTask = (task: Task)=> {
-      setTask(task);
+  // Função para fechar o modal de nova tarefa
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleSetTask = (task: Task) => {
+    setTask(task);
+    setModalOpen(true);
+  };
+
+  const handleSetActivePage = (page: string) => {
+    setActivePage(page);
+    if (page === "new") {
       setModalOpen(true);
     }
+  };
 
   return (
     <>
-      <Menu setActivePage={setActivePage} />
-      {activePage === "new" && <Modal open={modalOpen} onClose={closeModal} task={task}/>}
-      {task && <Modal open={modalOpen} onClose={closeModal} task={task}/>}
-      {activePage === "list" && <Painel activePage={activePage} setTask={handleSetTask} />}
+      <Menu setActivePage={handleSetActivePage} />
+      {activePage === "new" && (
+        <Modal
+          open={modalOpen}
+          onClose={closeModal}
+          task={{ title: "", description: "", status: "pending" }}
+        />
+      )}
+      {task && <Modal open={modalOpen} onClose={closeModal} task={task} />}
+      {activePage === "list" && (
+        <Painel activePage={activePage} setTask={handleSetTask} />
+      )}
     </>
   );
 }
